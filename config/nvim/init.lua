a3898cc0206c33fc84206913980415bac1e14c68
Matt Kobs <matt.kobs@cph.org>
2023-02-09 15:52:34 -0600

    Added ack and ripgrep searching to nvim
    Probably needs more fine-tuning before it’s ready for prime time…

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -48,6 +48,16 @@ command! -bang -nargs=* Rg
 ]])
 vim.keymap.set('n', '<C-g>', ':Rg!<CR>')
 
+-- Ack and Ripgrep searching
+vim.cmd([[
+  " Use ripgrep for searching
+  let g:ackprg = 'rg --vimgrep --smart-case'
+
+  " Any empty ack search will search for the word the cursor is on
+  let g:ack_use_cword_for_empty_search = 1
+]])
+vim.keymap.set('n', '<leader>g', ':Ack!<space>')
+
 -- Show invisible characters
 vim.opt.listchars = { eol = '¬', tab = '>·', trail = '~', extends = '>', precedes = '<', space = '·' }
 vim.opt.list = true
