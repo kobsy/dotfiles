4515f0f6cf7d4adc829b6cd8e2d845a0d75ee693
Matt Kobs <matt.kobs@cph.org>
2023-04-12 11:32:55 -0500

    [skip] Added nvim copilot plugin (1m)

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -43,6 +43,9 @@ return require('packer').startup(function(use)
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
 
+  -- Copilot!
+  use 'github/copilot.vim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
