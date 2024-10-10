44968fc8ed86c90945ff454f3d35c7dc369e2929
Matt Kobs <matt.kobs@cph.org>
2024-10-10 10:11:08 -0500

    More terminal font tweaks

--- wezterm.lua
+++ wezterm.lua
@@ -20,7 +20,8 @@ config.font = wezterm.font('IBM Plex Mono')
 -- config.font = wezterm.font('Zed Mono')
 
 config.font_size = 13.0
-config.freetype_load_target = 'Light'
+config.freetype_load_target = 'Normal'
+-- config.freetype_load_target = 'HorizontalLcd'
 
 -- Window configuration
 config.hide_tab_bar_if_only_one_tab = true
