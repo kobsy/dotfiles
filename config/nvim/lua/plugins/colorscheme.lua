37ce36760729093097335b2e1ba26e51b15e83d6
Matt Kobs <kobsy@hey.com>
2024-12-23 19:36:37 -0600

    Added dracula theme to LazyVim

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add dracula
+  { "dracula/vim", as = "dracula" },
+
+  -- Configure LazyVim to load dracula
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
