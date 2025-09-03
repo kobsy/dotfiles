927cb0273ed1543aa7f6455d21c8941bb2025652
Matt Kobs <matt.kobs@cph.org>
2025-09-03 08:38:49 -0500

    Added OpenWeather API key via 1Password CLI

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,35 +2,35 @@ fish_config theme choose "Dracula Official"
 
 # EP Toolchain
 if test -e ~/.ep/bin/ep
-  set -x PROJECTS_PATH "$HOME/Code"
-  eval (~/.ep/bin/ep init - fish)
+    set -x PROJECTS_PATH "$HOME/Code"
+    eval (~/.ep/bin/ep init - fish)
 else
-  source ~/.config/fish/aliases.fish
+    source ~/.config/fish/aliases.fish
 end
 
 # Homebrew on M1 Macs
 if test -e /opt/homebrew/bin/brew
-  eval (/opt/homebrew/bin/brew shellenv)
+    eval (/opt/homebrew/bin/brew shellenv)
 end
 
 # Rbenv Config
 if test -d "$HOME/.rbenv"
-  set -x RBENV_ROOT "$HOME/.rbenv"
-  status --is-interactive; and source (rbenv init -|psub)
+    set -x RBENV_ROOT "$HOME/.rbenv"
+    status --is-interactive; and source (rbenv init -|psub)
 end
 
 # Nodenv Config
-if which nodenv > /dev/null
-  status --is-interactive; and source (nodenv init -|psub)
+if which nodenv >/dev/null
+    status --is-interactive; and source (nodenv init -|psub)
 end
 
 # Pyenv Config
 if test -n "$PYENV_ROOT"
-  set -x PYENV_ROOT "$HOME/.pyenv"
-  set -x PATH "$PYENV_ROOT/bin" $PATH
+    set -x PYENV_ROOT "$HOME/.pyenv"
+    set -x PATH "$PYENV_ROOT/bin" $PATH
 end
-if which pyenv > /dev/null
-  status --is-interactive; and . (pyenv init -|psub)
+if which pyenv >/dev/null
+    status --is-interactive; and . (pyenv init -|psub)
 end
 
 # Allow finding the subl commmand
@@ -38,12 +38,12 @@ set -x PATH $PATH /Applications/Sublime\ Text.app/Contents/SharedSupport/bin
 
 # Preempt the path for postgres CLI tools
 if test -d /Applications/Postgres.app/Contents/Versions/latest/bin
-  set -x PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
+    set -x PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
 end
 
 # Add rust to the path
 if test -d ~/.cargo/bin
-  set -x PATH ~/.cargo/bin $PATH
+    set -x PATH ~/.cargo/bin $PATH
 end
 
 # Have fzf use ripgrep by default
@@ -55,12 +55,12 @@ set -x EDITOR "$VISUAL"
 
 # Add some override shims to PATH
 if test -d ~/.override_shims
-  set -x PATH ~/.override_shims $PATH
+    set -x PATH ~/.override_shims $PATH
 end
 
 # Add local python bin to PATH
 if test -d ~/.local/bin
-  set -x PATH ~/.local/bin $PATH
+    set -x PATH ~/.local/bin $PATH
 end
 
 # Make cd to common directories easier
@@ -69,12 +69,17 @@ set -x CDPATH . ~ ~/Sites ~/Code ~/Documents
 # Avoid a weird OBJC error with Ruby
 set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 set -g fish_user_paths "/usr/local/opt/postgresql@11/bin" $fish_user_paths
 
 # pnpm
-set -gx PNPM_HOME "/Users/kobsmc/Library/pnpm"
+set -gx PNPM_HOME /Users/kobsmc/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# For tmux-clima; credentials stored in 1Password
+if test -z "$OPEN_WEATHER_API_KEY" && which op >/dev/null
+    set -x OPEN_WEATHER_API_KEY (op item get b55fuwastwi5urgkljkbu73cqu --reveal --field credential --account BNZ3QABYSJBVJBHWEQQSE6MKRE)
+end
